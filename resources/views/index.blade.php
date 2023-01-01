8426a7093ed3b4e63bd069ad20bb7774ef15097b
Mobina Soleimani <mobinas296@gmail.com>
2023-01-01 23:56:50 +0330

    change dashbord my admin

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"crossorigin="anonymous">
-    <title>index</title>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <link src="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="path/to/fontawesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
+   
+  <title>index</title>
 </head>
 <body>
     <div>
@@ -15,7 +22,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#"><i class="fa-regular fa-cart-shopping"></i> <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i> <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active">
         <a class="nav-link" href="#">ورود | ثبت نام <i class="fa-solid fa-arrow-left-long-to-line"></i><span class="sr-only">(current)</span></a>
@@ -24,7 +31,7 @@
     </ul>
     <form class="form-inline my-2 my-lg-0 ">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <i class="fa-regular fa-magnifying-glass"></i>
+      <i class="fa-solid fa-magnifying-glass"></i>
     </form>
   </div>
 </nav> 
@@ -32,7 +39,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">لطفا شهر خود را انتخاب کنید <i class="fa-regular fa-location-dot"></i><span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">لطفا شهر خود را انتخاب کنید <i class="fa-solid fa-location-dot"></i><span class="sr-only">(current)</span></a>
     </ul>
     <form class="form-inline my-2 my-lg-0">
     <div class="collapse navbar-collapse pl-5 mr-1" id="navbarSupportedContent">
@@ -44,16 +51,16 @@
         <a class="nav-link " href="#">سوالی دارید؟ <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link " href="#">شگفت انگیزها <i class="fa-light fa-badge-percent"></i><span class="sr-only">(current)</span></a>
+        <a class="nav-link " href="#">شگفت انگیزها <i class="fa-sharp fa-solid fa-badge-percent"></i><span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active">
         <a class="nav-link " href="#">تخفیف ها و پیشنهادها <i class="fa-solid fa-percent"></i><span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link " href="#">پرفروش ترین ها <i class="fa-regular fa-fire-flame-curved"></i><span class="sr-only">(current)</span></a>
+        <a class="nav-link " href="#">پرفروش ترین ها <i class="fa-solid fa-fire-flame-curved"></i><span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link " href="#">سوپرمارکت <i class="fa-light fa-bags-shopping"></i><span class="sr-only">(current)</span></a>
+        <a class="nav-link " href="#">سوپرمارکت <i class="fa fa-shopping-bag" aria-hidden="true"></i><span class="sr-only">(current)</span></a>
       </li>
     </ul>
   </div>
@@ -61,58 +68,57 @@
   </div>
 </nav> 
 
-
 <div class="container-fluid">
     <div class="row flex-nowrap">
-        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-secondary">
             <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                 <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    <span class="fs-5 d-none d-sm-inline">دسته بندی کالاها</span>
+                    <h3 class="fs-5 d-none d-sm-inline">دسته بندی کالاها</h3>
                 </a>
                 <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                     <li class="nav-item">
-                        <a href="#" class="nav-link align-middle px-0">
+                        <a href="#" class="nav-link align-middle px-0 text-white">
                             <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">موبایل</span>
                         </a>
                     </li>
                     <li>
-                        <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                        <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle text-white">
                             <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">کالای دیجیتال</span> </a>
                     </li>                             
                             <li>
-                        <a href="#" class="nav-link px-0 align-middle">
+                        <a href="#" class="nav-link px-0 align-middle text-white">
                             <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">خودرو، ابزار و تجهیزات صنعتی</span></a>
                     </li>
                     <li>
-                        <a href="#" class="nav-link px-0 align-middle">
+                        <a href="#" class="nav-link px-0 align-middle text-white">
                             <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">مد و پوشاک</span></a>
                     </li>
                     <li>
-                        <a href="#" class="nav-link px-0 align-middle">
+                        <a href="#" class="nav-link px-0 align-middle text-white">
                             <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">کالاهای سوپرمارکتی</span></a>
                     </li>
                     <li>
-                        <a href="#" class="nav-link px-0 align-middle">
+                        <a href="#" class="nav-link px-0 align-middle text-white">
                             <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">اسباب بازی، کودک و نوزاد</span></a>
                     </li>
                     <li>
-                        <a href="#" class="nav-link px-0 align-middle">
+                        <a href="#" class="nav-link px-0 align-middle text-white">
                             <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline"> محصولات بومی و محلی</span></a>
                     </li>
                     <li>
-                        <a href="#" class="nav-link px-0 align-middle">
+                        <a href="#" class="nav-link px-0 align-middle text-white">
                             <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">زیبایی و سلامت</span></a>
                     </li>
                     <li>
-                        <a href="#" class="nav-link px-0 align-middle">
+                        <a href="#" class="nav-link px-0 align-middle text-white">
                             <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">خانه و آشپزخانه </span></a>
                     </li>
                     <li>
-                        <a href="#" class="nav-link px-0 align-middle">
+                        <a href="#" class="nav-link px-0 align-middle text-white">
                             <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">کتاب، لوازم تحریر و هنر</span></a>
                     </li>                      
                     <li>
-                        <a href="#" class="nav-link px-0 align-middle">
+                        <a href="#" class="nav-link px-0 align-middle text-white">
                             <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">ورزش و سفر </span></a>
                     </li>                                                           
                         </ul>
@@ -120,6 +126,7 @@
                 </ul>
             </div>
         </div>
+        @yield('content')
     </div>
 </div>
 </div>
