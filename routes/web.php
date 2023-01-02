9527cf9e3d1e98c9f6f6b5f23325d55b1ced7245
Mobina Soleimani <mobinas296@gmail.com>
2023-01-02 23:50:41 +0330

    login & register

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DigitalController;
+use App\Http\Controllers\CustomAuthController;
 
 Route::get('index', function () {
     return view('index');
 });
 
 Route::get('DigitalGoods',[DigitalController::class,'list']);
+Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom'); 
+Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
+Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom'); 
+Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
